db216a03c9625256ebbc8ae2b74edf29be187960
tallman5 <tallman5@users.noreply.github.com>
2024-02-25 14:01:38 -0500

    adding more utilities

---
+++ McGurkin/Runtime/Caching/CacheHelper.cs
@@ -0,0 +1,39 @@
+﻿using McGurkin.Runtime.Serialization;
+using System.Runtime.Caching;
+
+namespace McGurkin.Runtime.Caching
+{
+    public class CacheHelper
+    {
+        public static void Clear()
+        {
+            MemoryCache.Default.Trim(100);
+        }
+
+        public static T Get<T>(string cacheKey)
+        {
+            var deflated = MemoryCache.Default.Get(cacheKey) as string;
+            if (string.IsNullOrWhiteSpace(deflated))
+                throw new Exception($"An item with a key of {cacheKey} was not found in the cache or it's an empty string.");
+            var returnValue = Serializer.FromCompressedString<T>(deflated);
+            return returnValue;
+        }
+
+        public static void Remove(string cacheKey)
+        {
+            MemoryCache.Default.Remove(cacheKey);
+        }
+
+        public static void Set(string cacheKey, object o)
+        {
+            var expiration = DateTimeOffset.Now.AddHours(24);
+            Set(cacheKey, o, expiration);
+        }
+
+        public static void Set(string cacheKey, object o, DateTimeOffset expiration)
+        {
+            var deflated = Serializer.ToCompressedString(o);
+            MemoryCache.Default.Set(cacheKey, deflated, expiration);
+        }
+    }
+}
--- McGurkin/Runtime/Serialization/Serializer.cs
+++ McGurkin/Runtime/Serialization/Serializer.cs
@@ -1,4 +1,6 @@
-﻿using System.Text.Json;
+﻿using System.IO.Compression;
+using System.Text;
+using System.Text.Json;
 
 namespace McGurkin.Runtime.Serialization
 {
@@ -8,6 +10,31 @@ namespace McGurkin.Runtime.Serialization
         {
             var objJson = ToString(o);
             var returnValue = FromString<T>(objJson);
+            return returnValue;
+        }
+
+        public static T FromCompressedString<T>(string s)
+        {
+            byte[] gZipBuffer = Convert.FromBase64String(s);
+            string objectString = string.Empty;
+
+            using (var memoryStream = new MemoryStream())
+            {
+                int dataLength = BitConverter.ToInt32(gZipBuffer, 0);
+                memoryStream.Write(gZipBuffer, 4, gZipBuffer.Length - 4);
+
+                var buffer = new byte[dataLength];
+
+                memoryStream.Position = 0;
+                using (var gZipStream = new GZipStream(memoryStream, CompressionMode.Decompress))
+                {
+                    gZipStream.Read(buffer, 0, buffer.Length);
+                }
+
+                objectString = Encoding.UTF8.GetString(buffer);
+            }
+
+            T? returnValue = FromString<T>(objectString);
             return returnValue;
         }
 
@@ -38,6 +65,35 @@ namespace McGurkin.Runtime.Serialization
                 }
                 return _Options;
             }
+        }
+
+        public static string ToCompressedString(object o)
+        {
+            string inflatedString = ToString(o);
+
+            byte[] buffer = Encoding.UTF8.GetBytes(inflatedString);
+            byte[] gZipBuffer;
+
+            using (var memoryStream = new MemoryStream())
+            {
+                using (var gZipStream = new GZipStream(memoryStream, CompressionMode.Compress, true))
+                {
+                    gZipStream.Write(buffer, 0, buffer.Length);
+                }
+
+                memoryStream.Position = 0;
+
+                var compressedData = new byte[memoryStream.Length];
+                memoryStream.Read(compressedData, 0, compressedData.Length);
+
+                gZipBuffer = new byte[compressedData.Length + 4];
+
+                Buffer.BlockCopy(compressedData, 0, gZipBuffer, 4, compressedData.Length);
+                Buffer.BlockCopy(BitConverter.GetBytes(buffer.Length), 0, gZipBuffer, 0, 4);
+            }
+
+            string returnValue = Convert.ToBase64String(gZipBuffer);
+            return returnValue;
         }
 
         public static string ToString(object o)
