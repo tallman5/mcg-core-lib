eadf419735c9e8e8c0a2b00be11646fe7ddcf5bd
tallman5 <tallman5@users.noreply.github.com>
2025-02-07 08:32:25 -0500

    removing email test

--- McGurkin.Test/IntegrationTest.cs
+++ McGurkin.Test/IntegrationTest.cs
@@ -1,21 +1,18 @@
-﻿using McGurkin.Services;
-using Microsoft.Extensions.Configuration;
+﻿//namespace McGurkin.Test;
 
-namespace McGurkin.Test;
-
-[TestClass]
-public class IntegrationTest : BaseTest
-{
-    [TestMethod]
-    public async Task EmailTestAsync()
-    {
-        var testTo = _configuration["TestTo"];
-        if (!string.IsNullOrWhiteSpace(testTo))
-        {
-            var esc = EmailSenderConfig.GetEmpty();
-            _configuration.GetSection("EmailSenderConfig").Bind(esc);
-            var es = new EmailSender(esc, _emailLogger);
-            await es.SendEmailAsync(testTo, "Testing", "This station is conducting a test.");
-        }
-    }
-}
+//[TestClass]
+//public class IntegrationTest : BaseTest
+//{
+//    [TestMethod]
+//    public async Task EmailTestAsync()
+//    {
+//        var testTo = _configuration["TestTo"];
+//        if (!string.IsNullOrWhiteSpace(testTo))
+//        {
+//            var esc = EmailSenderConfig.GetEmpty();
+//            _configuration.GetSection("EmailSenderConfig").Bind(esc);
+//            var es = new EmailSender(esc, _emailLogger);
+//            await es.SendEmailAsync(testTo, "Testing", "This station is conducting a test.");
+//        }
+//    }
+//}
